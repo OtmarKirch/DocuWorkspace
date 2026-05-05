4773f12bf113829c931b2b4ef4b7d22caae0e167
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2026-05-05 16:47:59 +0200

    Add seperate sections folder

--- Documentation/clean-build.sh
+++ Documentation/clean-build.sh
@@ -6,5 +6,6 @@ cd "$(dirname "$0")"
 # Remove all generated files from the build directory, then rebuild from scratch.
 mkdir -p build
 find build -mindepth 1 -maxdepth 1 -exec rm -rf {} +
+mkdir -p build/sections
 
 latexmk -xelatex main.tex
